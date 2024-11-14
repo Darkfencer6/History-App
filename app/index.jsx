584b3b9bf7bcdf7a3c75c5fb933f18fb52364b90
Adriane M. Gumahad <70140170+Darkfencer6@users.noreply.github.com>
2024-11-15 02:08:29 +0800

    index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import {ScrollView, Text, View, Image } from 'react-native';
 import { Link, Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { images } from '../constants';
+import { images } from '../assets/constants/images';
 import CustomButton from '../Components/CustomButton';
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
       <ScrollView contentContainerStyle={{height:'100%'}}>
         <View className="w-full justify-center, items-center min-h-screenh-[85vh] px-4">
               <Image
-                source={images.capsule}
+                source={images.logo}
                 className="w-[300px] h-[84px] pt-10"
                 resizeMode='contain'
               />
@@ -22,10 +22,10 @@ export default function App() {
               />
               <View className="relative mt-5">
                   <Text className="text-2xl text-white font-u_bold text-center">
-                    A Display of Student Innovative Portfolio with Audience-Driven Recognition
+                    Welcome To History App You can Learn Every thing that you dont know the past!
                   </Text>
               </View>
-            <Text className="text-sm, font-j_bold text-gray-100 mt-7 text-center"> Join us, Let's create One Portfolio at a time!
+            <Text className="text-sm, font-j_bold text-gray-100 mt-7 text-center"> Join us, Let's Discover Together!
             </Text>
             <CustomButton
               title="Continue with Email"
